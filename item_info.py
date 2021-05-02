9fb3ed87ebea9953c37c3342981d83391996ed23
mn701 <46086846+mn701@users.noreply.github.com>
2021-05-02 20:45:24 +0800

    [first commit]

---
+++ item_info.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+def getItemInfo(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html, 'lxml')
+        title = bsObj.h1.string
+        sku = bsObj.find("span", {"itemprop":{"productID"}}).get_text()
+        price = bsObj.find("meta", {"itemprop":{"price"}})['content']
+        description = bsObj.find("div", {"itemprop":{"description"}}).get_text()
+        details = bsObj.find("div", {"class":"js-product-details_val"}).get_text()
+    except AttributeError as e:
+        return None
+    print('title: ' + title + '\n' +
+        'sku: ' + sku + '\n' +
+        'price: ' + price + '\n' +
+        'description: ' + description + '\n' +
+        'details: ' + details)
+
+getItemInfo("https://www.pedroshoes.com/sg/women/PW2-75060043_GREEN.html")
