832e9da3c642939f84ac40848e27d596af3a5761
mn701 <46086846+mn701@users.noreply.github.com>
2021-05-05 17:04:54 +0800

    [added] storing to mysql
    some refactoring

--- item_info.py
+++ item_info.py
@@ -4,6 +4,12 @@ from bs4 import BeautifulSoup
 from urllib.request import urlretrieve
 import re
 import os
+import pymysql
+
+# Connect MySQL
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock', user='root', passwd='wawa1234', db='mysql', charset='utf8')
+cur = conn.cursor()
+cur.execute("USE shop")
 
 def getItemInfo(url):
     try:
@@ -12,32 +18,47 @@ def getItemInfo(url):
         return None
     try:
         bsObj = BeautifulSoup(html, 'lxml')
-        title = bsObj.h1.string
         sku = bsObj.find("span", {"itemprop":{"productID"}}).get_text()
+        title = bsObj.h1.string
         price = bsObj.find("meta", {"itemprop":{"price"}})['content']
         description = bsObj.find("div", {"itemprop":{"description"}}).get_text().strip()
         details = bsObj.find("div", {"class":"js-product-details_val"}).get_text().strip()
         color = bsObj.find("div", {"class":{"selected-color"}}).get_text().strip()
 
         sku_short = re.findall("\w+\d-\d+", sku)[0]
+        brand_id = '2'
 
-        print_item(sku_short, url, title, price, description, details)
-        pritn_variant(sku, url, color)
+        # print_item(sku_short, url, title, price, description, details)
+        # print_variation(sku, url, color)
 
+        try:
+            store_item(brand_id, sku_short, url, title, price, description, details)
+            cur.execute("SELECT item_id FROM items WHERE serial='" + sku_short + "'")
+            if(cur.rowcount > 0):
+                item_id = cur.fetchone()[0]
+                store_variation(str(item_id), sku, url)
+        finally:
+            cur.close()
+            conn.close()
+
+        arr_img = bsObj.findAll("img", {"itemprop":"image"})
         # Creating a folder using sku
-        folderName = sku_short
-        currPath = os.path.dirname(os.path.realpath(__file__))
-        reqPath = os.path.join(currPath,folderName)
-        if os.path.isdir(reqPath) == False:
-            os.mkdir(folderName)
-
-        for img in bsObj.findAll("img", {"itemprop":"image"}):
-            imglocation = img['src']
-            imgname= re.findall("[\d, \w,-]+\.jpg", imglocation)[0]
-            filename = os.path.join(reqPath, imgname)
-            urlretrieve(imglocation, filename)
+        foldername = sku_short
+        save_imgs(arr_img, foldername)
     except AttributeError as e:
         return None
+
+# download images in the folder
+def save_imgs(images, folderName):
+    currPath = os.path.dirname(os.path.realpath(__file__))
+    reqPath = os.path.join(currPath,folderName)
+    if os.path.isdir(reqPath) == False:
+        os.mkdir(folderName)
+    for img in images:
+        imglocation = img['src']
+        imgname= re.findall("[\d, \w,-]+\.jpg", imglocation)[0]
+        filename = os.path.join(reqPath, imgname)
+        urlretrieve(imglocation, filename)
 
 def print_item(product_id, url, product_name, price, description, details):
     print('product_id: '+ product_id + '\n' +
@@ -46,10 +67,29 @@ def print_item(product_id, url, product_name, price, description, details):
         'price: ' + price + '\n' +
         'description: ' + description + '\n' +
         'details: ' + details+ '\n')
-def pritn_variant(sku, url, color):
+
+def print_variation(sku, url, color):
     print('variant sku: '+ sku + '\n' +
     'variant url: ' + url + '\n' +
     'color: ' + color)
+
+# Storing item info into  MySQL database
+def store_item(brand_id, serial, url, item_name, price, description, details):
+    cur.execute("SELECT * FROM Items WHERE serial='" + serial + "'")
+    exist = cur.fetchone()
+    if exist is None:
+        cur.execute("INSERT INTO Items (brand_id, serial, url, item_name, price, description, details) VALUES ('" + brand_id + "','" + serial + "','" + url + "','" + item_name  + "','" + price  + "','" + description + "','" + details + "')")
+        cur.connection.commit()
+
+# Storing items' Variation into Variations table
+def store_variation(item_id, sku, url):
+    cur.execute("SELECT * FROM Variations WHERE sku='" + sku + "'")
+    exist = cur.fetchone()
+    if exist is None:
+        cur.execute("INSERT INTO Variations (item_id, sku, url, has_stock) VALUES ('" + item_id + "','" + sku + "','" + url + "', 1)")
+        cur.connection.commit()
+    else:
+        print("Item already exist!")
 
 print("Enter your url:")
 url = input()
