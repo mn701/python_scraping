bea32ede3e4dc68cebaa85299a759251dfa435a8
mn701 <46086846+mn701@users.noreply.github.com>
2021-05-15 12:09:23 +0800

    [added] createTop to create new image using pillow

--- imagefolder.py
+++ imagefolder.py
@@ -1,14 +1,43 @@
+from PIL import Image
 import os
 import shutil
+
+def createTop():
+    images = []
+    for filename in os.listdir():
+        if filename.endswith('.jpg'):
+            images.append(filename)
+
+    newImage = Image.new('RGBA', (1536,1536))
+
+    quarterImages = []
+    for imgfile in images:
+        imgName = imgfile[-8:-4]
+        copyIm = Image.open(imgfile).copy()
+        width, height = copyIm.size
+        quartersizedIm = copyIm.resize((int(width / 2),int(height / 2)))
+        # quartersizedIm.save(imgName + 'quartersized.png')
+        quarterImages.append(quartersizedIm)
+
+    coords = []
+    for left in [0, 768]:
+        for top in [0, 768]:
+            coords.append((left, top))
+
+    for quarterIm in quarterImages:
+        newImage.paste(quarterIm, coords[quarterImages.index(quarterIm)])
+
+    newImage.save('newImage.png')
 
 directory = os.getcwd()
 for filename in os.scandir(directory):
     if os.path.isdir(filename):
         os.chdir(filename.path)
 
-        cwd = os.getcwd()
         if not os.path.exists('top'):
             os.mkdir("top")
-            for filename in os.listdir(cwd):
+            for filename in os.listdir():
                 if filename.endswith("-1.jpg"):
                     shutil.copy(filename, "top")
+        os.chdir("top")
+        createTop()
