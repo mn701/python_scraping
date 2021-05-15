f635a748c471bd40c194fddb73083135168c7e2d
mn701 <46086846+mn701@users.noreply.github.com>
2021-05-15 15:05:27 +0800

    Update imagefolder.py
    [refactoring]

--- imagefolder.py
+++ imagefolder.py
@@ -2,33 +2,53 @@ from PIL import Image
 import os
 import shutil
 
-def createTop():
+SQUARE_SIZE = 1536
+SQUARE_HALF = 768
+
+def create_top(images):
+    newImage = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE))
+
+    # Create a list for quaretersized images.
+    quarterImages = []
+    for imgfile in images:
+        copyIm = Image.open(imgfile).copy()
+        crop_rectangle = (0, 192, 1152, 1344)
+        cropped_im = copyIm.crop(crop_rectangle)
+        quartersizedIm = cropped_im.resize((SQUARE_HALF, SQUARE_HALF))
+        quarterImages.append(quartersizedIm)
+
+    # Create a list of coordinates.
+    coords = []
+    for left in [0, SQUARE_HALF]:
+        for top in [0, SQUARE_HALF]:
+            coords.append((left, top))
+            
+    # Paste each quartersized image to newImage
+    for quarterIm in quarterImages:
+        newImage.paste(quarterIm, coords[quarterImages.index(quarterIm)])
+
+    add_logo(newImage)
+
+def add_logo(image):
+    # Adding logo image
+    LOGO_FILENAME = 'catlogo.png'
+    logoIm = Image.open(LOGO_FILENAME)
+    logoIm = logoIm.resize((200, 200))
+    logoWidth, logoHeight = logoIm.size
+
+    image.paste(logoIm, (SQUARE_SIZE-logoWidth, SQUARE_SIZE-logoHeight), logoIm)
+    
+    image.save('imageWithLogo.png')
+
+def get_images():
     images = []
     for filename in os.listdir():
         if filename.endswith('.jpg'):
             images.append(filename)
 
     if len(images) > 0:
-        newImage = Image.new('RGBA', (1536,1536))
+        create_top(images)
 
-        quarterImages = []
-        for imgfile in images:
-            imgName = imgfile[-8:-4]
-            copyIm = Image.open(imgfile).copy()
-            width, height = copyIm.size
-            quartersizedIm = copyIm.resize((int(width / 2),int(height / 2)))
-            # quartersizedIm.save(imgName + 'quartersized.png')
-            quarterImages.append(quartersizedIm)
-
-        coords = []
-        for left in [0, 768]:
-            for top in [0, 768]:
-                coords.append((left, top))
-
-        for quarterIm in quarterImages:
-            newImage.paste(quarterIm, coords[quarterImages.index(quarterIm)])
-
-        newImage.save('newImage.png')
 
 # mask directory name
 directory = os.getcwd()
@@ -42,4 +62,4 @@ for filename in os.scandir(directory):
                 if filename.endswith('-1.jpg'):
                     shutil.copy(filename, 'top')
         os.chdir('top')
-        createTop()
+        get_images()
