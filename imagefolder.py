7b85997d4e2c153277d0b1503b437fca5bfa71ec
mn701 <46086846+mn701@users.noreply.github.com>
2021-05-15 20:10:48 +0800

    [added] 2 vertical images [added] add lot top

--- imagefolder.py
+++ imagefolder.py
@@ -4,16 +4,19 @@ import shutil
 
 SQUARE_SIZE = 1536
 SQUARE_HALF = 768
-LOGO_FILENAME = 'catlogo.png'
+LOGO_SHOP = 'logo_200x200.png'
+PEDRO_LOGO = 'logo.png'
 
 def create_top(images):
     new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE))
     if(len(images) > 2 and len(images) < 5):
         new_im = create_im_from_4(images)
         new_im = add_logo_center(new_im)
-    elif(len(images) == 1):
+    elif(len(images) > 1):
+        new_im = create_im_2_ver(images)
+    elif(len(images) > 0):
         new_im = create_im_1(images[0])
-        new_im = add_logo_center(new_im)
+        new_im = add_logo_top(new_im)
 
     im_with_logo = add_shop_logo(new_im)
     im_with_logo.save('with_logo.png')
@@ -48,10 +51,40 @@ def create_im_from_4(images):
 
     return new_im
 
+def create_im_2_ver(images):
+    X_CROP = 100
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE))
+
+    # Create a list for two vertical images.
+    resized_images = []
+    for imgfile in images:
+        copy_im = Image.open(imgfile).copy()
+        x, y = copy_im.size
+        crop_rectangle = (X_CROP, 0, int(x - 100), y)
+        cropped_im = copy_im.crop(crop_rectangle)
+        cropped_x, cropped_y = cropped_im.size
+        new_width  = SQUARE_HALF
+        new_height = int(SQUARE_HALF * cropped_y / cropped_x)
+        resized_im = cropped_im.resize((new_width, new_height))
+        resized_im.save('resized' + imgfile)
+        resized_images.append(resized_im)
+
+    # Create a list of coordinates.
+    coords = []
+    for left in [0, SQUARE_HALF]:
+        top = SQUARE_HALF - int(new_height / 2)
+        coords.append((left, top))
+
+    # Paste each quartersized image to new_im
+    for im in resized_images:
+        new_im.paste(im, coords[resized_images.index(im)])
+
+    return new_im
+
 def add_shop_logo(image):
     # Adding logo image
-    logo_im = Image.open(LOGO_FILENAME)
-    logo_im = logo_im.resize((200, 200))
+    logo_im = Image.open(LOGO_SHOP)
+    logo_im = logo_im.resize((267, 267))
     logoWidth, logoHeight = logo_im.size
     width, height = image.size
 
@@ -60,15 +93,27 @@ def add_shop_logo(image):
 
     return image
 
-def add_logo_center(image):
+def add_logo_top(image):
     # Adding logo image
-    logo_im = Image.open(LOGO_FILENAME)
-    logo_im = logo_im.resize((200, 200))
+    logo_im = Image.open(PEDRO_LOGO).convert("RGBA")
+    logo_im = logo_im.resize((600, 112))
     logoWidth, logoHeight = logo_im.size
     width, height = image.size
 
     # Paste logo on botom left
-    image.paste(logo_im, (int(width/2) - int(logoWidth/2), int(height/2) - int(logoHeight/2)), logo_im)
+    image.paste(logo_im, (468, 20), logo_im)
+
+    return image
+
+def add_logo_center(image):
+    # Adding logo image
+    logo_im = Image.open(PEDRO_LOGO).convert("RGBA")
+    logo_im = logo_im.resize((600, 112))
+    logoWidth, logoHeight = logo_im.size
+    width, height = image.size
+
+    # Paste logo on botom left
+    image.paste(logo_im, ((int(width/2) - int(logoWidth/2)), (int(height/2) - int(logoHeight/2))), logo_im)
 
     return image
 
