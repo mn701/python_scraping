5ad44af025f1c47a8bafe2cfe27dc4c45d04589d
mn701 <46086846+mn701@users.noreply.github.com>
2021-05-16 21:57:15 +0800

    first commit

---
+++ crawling.py
@@ -0,0 +1,39 @@
+import pymysql
+import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock',user='root', passwd=None, db='mysql')
+
+cur = conn.cursor()
+cur.execute("USE shop")
+cur.execute("SELECT id, url FROM variations where has_stock = 1")
+rows = cur.fetchall()
+print(cur.rowcount)
+for row in rows:
+    res = requests.get(row[1])
+    try:
+        res.raise_for_status()
+    except Exception as e:
+        print('There was a problem: %s %s' % (e, row[1]))
+    else:
+        html = urlopen(row[1])
+        bsObj = BeautifulSoup(html.read())
+        txt_availability = ''
+        unavailable = bsObj.find("div", {"class":"product_retirement-unavailable_text"})
+        if unavailable:
+            txt_availability = 'Unavailable'
+        else:
+            availability = bsObj.find("span", {"class":"pdp-availability_badge"})
+            if availability:
+                txt_availability = availability.get_text().strip()
+            else:
+                txt_availability = 'No Content'
+            id = str(row[0])
+        sql = "UPDATE variations SET availability = '" + txt_availability + "' WHERE id = '" + id + "'"
+
+        cur.execute(sql)
+        conn.commit()
+
+cur.close()
+conn.close()
