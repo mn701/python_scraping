4d8553678b832dfb3dfe6c7dc74d0c06885749dc
mn701 <46086846+mn701@users.noreply.github.com>
2021-05-21 19:29:56 +0800

    [added] logging

--- crawling.py
+++ crawling.py
@@ -4,6 +4,10 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import os
 
+#logging
+log_format = '%(asctime)s %(filename)s: %(message)s'
+logging.basicConfig(filename='crawling.log', level=logging.DEBUG, format=log_format)
+
 conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock',user='root', passwd=os.environ['mysql_password'], db='mysql')
 
 cur = conn.cursor()
@@ -16,7 +20,7 @@ for row in rows:
     try:
         res.raise_for_status()
     except Exception as e:
-        print('There was a problem: %s %s' % (e, row[1]))
+        logging.warning("There was a problem: %s %s", e, row[1])
     else:
         html = urlopen(row[1])
         bsObj = BeautifulSoup(html, 'lxml')
@@ -24,12 +28,14 @@ for row in rows:
         unavailable = bsObj.find("div", {"class":"product_retirement-unavailable_text"})
         if unavailable:
             txt_availability = 'Unavailable'
+            logging.warning("Unavailable: %s", row[1])
         else:
             availability = bsObj.find("span", {"class":"pdp-availability_badge"})
             if availability:
                 txt_availability = availability.get_text().strip()
             else:
                 txt_availability = 'No Content'
+                logging.warning("No availability info: %s", row[1])
             id = str(row[0])
         sql = "UPDATE variations SET availability = '" + txt_availability + "' WHERE id = '" + id + "'"
 
