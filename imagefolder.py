ed82b15bf01bd6b005f7381d1ea3d73249c3b9eb
mn701 <46086846+mn701@users.noreply.github.com>
2021-05-26 12:38:56 +0800

    [updated] background color

--- imagefolder.py
+++ imagefolder.py
@@ -7,44 +7,48 @@ SQUARE_HALF = 768
 LOGO_SHOP = 'logo_200x200.png'
 PEDRO_LOGO = 'logo.png'
 
-def create_top(images):
+def create_top(images, brand):
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE))
     if len(images) == 4:
-        create_im_from_4(images, 100).save('100.png')
-        create_im_from_4(images, 150).save('150.png')
-        create_im_from_4(images, 200).save('200.png')
+        create_im_from_4(images, brand, 100).save('100.png')
+        create_im_from_4(images, brand, 150).save('150.png')
+        create_im_from_4(images, brand, 200).save('200.png')
     elif len(images) == 3:
-        create_im_3_ver(images, 100).save('100v.png')
-        create_im_3_ver(images, 200).save('200v.png')
-        create_im_3_hor(images, 100).save('100h.png')
-        create_im_3_hor(images, 200).save('200h.png')
-        create_im_3_hor(images, 250).save('250h.png')
-        create_im_3_hor(images, 300).save('300h.png')
+        create_im_3_ver(images, brand, 100).save('100v.png')
+        create_im_3_ver(images, brand, 200).save('200v.png')
+        create_im_3_hor(images, brand, 100).save('100h.png')
+        create_im_3_hor(images, brand, 200).save('200h.png')
+        create_im_3_hor(images, brand, 250).save('250h.png')
+        create_im_3_hor(images, brand, 300).save('300h.png')
     elif len(images) == 2:
-        create_im_2_ver(images, 100).save('100v.png')
-        create_im_2_ver(images, 200).save('200v.png')
-        create_im_2_hor(images, 100).save('100h.png')
-        create_im_2_hor(images, 200).save('200h.png')
-        create_im_2_hor(images, 250).save('250h.png')
+        create_im_2_ver(images, brand, 100).save('100v.png')
+        create_im_2_ver(images, brand, 200).save('200v.png')
+        create_im_2_hor(images, brand, 100).save('100h.png')
+        create_im_2_hor(images, brand, 200).save('200h.png')
+        create_im_2_hor(images, brand, 250).save('250h.png')
     elif len(images) == 1:
-        create_im_1(images[0], 0).save('0.png')
-        create_im_1(images[0], 50).save('50.png')
-        create_im_1(images[0], 100).save('100.png')
+        create_im_1(images[0], brand, 0).save('0.png')
+        create_im_1(images[0], brand, 50).save('50.png')
+        create_im_1(images[0], brand, 100).save('100.png')
 
-def create_im_1(image, crop_size=0):
+def create_im_1(image, brand, crop_size=0):
     copy_im = Image.open(image).copy()
     crop_rectangle = (0 + crop_size, 192 + crop_size, 1152 - crop_size, 1344 - crop_size)
     cropped_im = copy_im.crop(crop_rectangle)
     resized = cropped_im.resize((SQUARE_SIZE, SQUARE_SIZE))
 
-    # Add logo at center
-    new_im = add_logo_top(resized)
+    # Add logo at top
+    if brand == 1:
+        new_im = add_cklogo_top(resized)
+    elif brand == 2:
+        new_im = add_logo_top(resized)
 
     # Add shop logo
     im_with_logo = add_shop_logo(new_im)
 
     return im_with_logo
 
-def create_im_from_4(images, crop_size=0):
+def create_im_from_4(images, brand, crop_size=0):
     new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE))
     # Create a list for quaretersized images.
     quarter_images = []
@@ -65,15 +69,15 @@ def create_im_from_4(images, crop_size=0):
         new_im.paste(quarter_im, coords[quarter_images.index(quarter_im)])
 
     # Add logo at center
-    new_im = add_logo_center(new_im)
+    new_im = add_logo_center(new_im, brand)
 
     # Add shop logo
     im_with_logo = add_shop_logo(new_im)
 
     return im_with_logo
 
-def create_im_2_ver(images, crop_x_size=100):
-    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE))
+def create_im_2_ver(images, brand, crop_x_size=100):
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE),(236, 236, 236))
     new_width = SQUARE_HALF
     # Create a new folder to save resized images.
     os.makedirs('vresized', exist_ok=True)
@@ -92,16 +96,19 @@ def create_im_2_ver(images, crop_x_size=100):
     for im in resized_images:
         new_im.paste(im, coords[resized_images.index(im)])
 
-    # Add logo at center
-    new_im = add_logo_top(new_im)
+    # Add logo at top
+    if brand == 1:
+        new_im = add_cklogo_top(new_im)
+    elif brand == 2:
+        new_im = add_logo_top(new_im)
 
     # Add shop logo
     im_with_logo = add_shop_logo(new_im)
 
     return im_with_logo
 
-def create_im_3_ver(images, crop_x_size=100):
-    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE))
+def create_im_3_ver(images, brand, crop_x_size=100):
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (236, 236, 236))
     new_width = int(SQUARE_SIZE / 3)
     # Create a new folder to save resized images.
     os.makedirs('vresized', exist_ok=True)
@@ -121,15 +128,18 @@ def create_im_3_ver(images, crop_x_size=100):
         new_im.paste(im, coords[resized_images.index(im)])
 
     # Add logo at top
-    new_im = add_logo_top(new_im)
+    if brand == 1:
+        new_im = add_cklogo_top(new_im)
+    elif brand == 2:
+        new_im = add_logo_top(new_im)
 
     # Add shop logo
     im_with_logo = add_shop_logo(new_im)
 
     return im_with_logo
 
-def create_im_2_hor(images, crop_y_size=100):
-    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE))
+def create_im_2_hor(images, brand, crop_y_size=100):
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE),(236, 236, 236))
     new_height = SQUARE_HALF
     # Create a new folder to save resized images.
     os.makedirs('hresized', exist_ok=True)
@@ -149,15 +159,15 @@ def create_im_2_hor(images, crop_y_size=100):
         new_im.paste(im, coords[resized_images.index(im)])
 
     # Add logo at center
-    new_im = add_logo_center(new_im)
+    new_im = add_logo_center(new_im, brand)
 
     # Add shop logo
     im_with_logo = add_shop_logo(new_im)
 
     return im_with_logo
 
-def create_im_3_hor(images, crop_y_size=100):
-    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE))
+def create_im_3_hor(images, brand, crop_y_size=100):
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (236, 236, 236))
     new_height = int(SQUARE_SIZE / 3)
     # Create a new folder to save resized images.
     os.makedirs('hresized', exist_ok=True)
@@ -177,7 +187,8 @@ def create_im_3_hor(images, crop_y_size=100):
         new_im.paste(im, coords[resized_images.index(im)])
 
     # Add logo on the left
-    new_im = add_logo_left(new_im)
+    new_im = add_logo_left(new_im, brand)
+
     # Add shop logo
     im_with_logo = add_shop_logo(new_im)
 
@@ -224,7 +235,7 @@ def crop_y_bottom(im, crop_size):
     crop_square = (0, y - x, x, y)
     square_im = im.crop(crop_square)
     sq_x, sq_y = square_im.size
-    crop_rectangle = (0, crop_size, sq_x, int(sq_y - crop_size))
+    crop_rectangle = (0, crop_size, sq_x, int(sq_y - crop_size*0.8))
     cropped_im = square_im.crop(crop_rectangle)
     return cropped_im
 
@@ -261,10 +272,15 @@ def add_logo_top(image):
 
     return image
 
-def add_logo_center(image):
+def add_logo_center(image, brand):
     # Adding logo image
-    logo_im = Image.open(PEDRO_LOGO).convert("RGBA")
-    logo_im = logo_im.resize((600, 112))
+    if brand == 2:
+        logo_im = Image.open(PEDRO_LOGO).convert("RGBA")
+        logo_im = logo_im.resize((600, 112))
+    else:
+        logo_im = Image.open(CK_LOGO).convert("RGBA")
+        logo_im = logo_im.resize((400, 400))
+
     logoWidth, logoHeight = logo_im.size
     width, height = image.size
 
@@ -273,38 +289,57 @@ def add_logo_center(image):
 
     return image
 
-def add_logo_left(image):
+def add_logo_left(image, brand):
     # Adding logo image
-    logo_im = Image.open(PEDRO_LOGO).convert("RGBA")
-    logo_im = logo_im.resize((600, 112))
+    if brand == 1:
+        logo_im = Image.open(CK_LOGO).convert("RGBA")
+        logo_im = logo_im.resize((400, 400))
+        # Paste logo on botom left
+        image.paste(logo_im, (0, 568), logo_im)
+    else:
+        logo_im = Image.open(PEDRO_LOGO).convert("RGBA")
+        logo_im = logo_im.resize((600, 112))
+        # Paste logo on botom left
+        image.paste(logo_im, (15, 712), logo_im)
+
+    return image
+
+def add_cklogo_top(image):
+    # Adding logo image
+    logo_im = Image.open(CK_LOGO).convert("RGBA")
+    logo_im = logo_im.resize((400, 400))
     logoWidth, logoHeight = logo_im.size
     width, height = image.size
 
     # Paste logo on botom left
-    image.paste(logo_im, (15, 712), logo_im)
+    image.paste(logo_im, (568, 0), logo_im)
 
     return image
-def get_images():
+
+
+def get_images(brand):
     images = []
     for filename in os.listdir():
         if filename.endswith('.jpg') and filename.startswith('20'):
             images.append(filename)
 
     if len(images) > 0:
-        create_top(images)
-
+        create_top(images, brand)
 
 # mask directory name
 directory = os.getcwd()
 for filename in os.scandir(directory):
-    if os.path.isdir(filename) and 'PW' in str(filename):
+    if os.path.isdir(filename) and ('PW' in str(filename) or 'CK' in str(filename)):
         os.chdir(filename.path)
-
+        if 'PW' in str(filename):
+            brand = 2
+        elif 'CK' in str(filename):
+            brand = 1
         if not os.path.exists('top'):
             os.mkdir('top')
             for filename in os.listdir():
                 if filename.endswith('-1.jpg'):
-                    shutil.copy(filename, 'top')
+                    shutil.move(filename, os.path.join('top', filename))
         if len([name for name in os.listdir() if os.path.isfile(name)]) > 19:
             if not os.path.exists('extra'):
                 os.mkdir('extra')
@@ -312,4 +347,4 @@ for filename in os.scandir(directory):
                 if filename.endswith('-8.jpg'):
                     shutil.move(filename, os.path.join('extra', filename))
         os.chdir('top')
-        get_images()
+        get_images(brand)
