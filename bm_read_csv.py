3e13310eb55c4b3841a389297d80c7e2608f734b
mn701 <46086846+mn701@users.noreply.github.com>
2021-06-05 20:24:33 +0800

    first commit

---
+++ bm_read_csv.py
@@ -0,0 +1,68 @@
+import pymysql
+import pandas as pd
+import os
+
+# connect to db
+pw = os.environ.get('mysql_password')
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock',user='root', passwd=pw, db='mysql')
+cur = conn.cursor()
+cur.execute("USE shop")
+df = pd.read_csv('/Users/MN1/Documents/Buyma/downloads_zaiko_only/colorsizes.utf8.csv', dtype=str)
+
+cur.execute("SELECT * FROM variations")
+rows = cur.fetchall()
+
+for row in rows:
+    item_id_str = str(row[1])
+    col_name = row[10]
+    try:
+        selected_variation = df.loc[(df['商品管理番号'] == item_id_str) & (df['色名称'] == col_name)]
+        new_stock = selected_variation['在庫ステータス'].values[0]
+        sql = "UPDATE variations SET has_stock = '" + new_stock + "' WHERE id = '" + str(row[0]) + "'"
+        cur.execute(sql)
+        if row[12] == null:
+            bm_col_family = selected_variation['色系統'].values[0]
+            sql = "UPDATE variations SET bm_col_family = '" + bm_col_family + "', '"\
+            + "is_listed = 1 "\
+            + " WHERE id = '" + str(row[0]) + "'"
+            cur.execute(sql)
+        conn.commit()
+    except IndexError:
+        pass
+
+df = pd.read_csv('/Users/MN1/Documents/Buyma/downloads_zaiko_only/items.utf8.csv', dtype=str)
+
+cur.execute("SELECT * FROM Listed_items")
+rows = cur.fetchall()
+for row in rows:
+    item_id_str = str(row[1])
+
+    try:
+        selected_item = df.loc[(df['商品管理番号'] == item_id_str)]
+        bm_id = selected_item['商品ID'].values[0]
+        bm_control = selected_item['コントロール'].values[0]
+        listed_name = selected_item['商品名'].values[0]
+        sale_price = selected_item['単価'].values[0]
+        bm_pcs = selected_item['買付可数量'].values[0]
+        valid_till = selected_item['購入期限'].values[0]
+        bm_has_refprice = selected_item['参考価格/通常出品価格'].values[0]
+        if bm_id != row[13]:
+            sql = "UPDATE Listed_items SET buyma_id = '" + bm_id + "', "\
+                + "listed_name = '" + listed_name + "', "\
+                + "sale_price = '" + sale_price + "', "\
+                + "bm_pcs = '" + bm_pcs + "', "\
+                + "bm_has_refprice = '" + bm_has_refprice + "' "\
+                + " WHERE id = " + str(row[0])
+            cur.execute(sql)
+
+        sql = "UPDATE Listed_items SET buyma_id = '" + bm_id + "', "\
+            + "bm_control = '" + bm_control + "', "\
+            + "valid_till = '" + valid_till + "' "\
+            + " WHERE id = " + str(row[0])
+        cur.execute(sql)
+        conn.commit()
+    except IndexError:
+        pass
+
+cur.close()
+conn.close()
---
+++ bm_write_csv.py
@@ -0,0 +1,55 @@
+import csv
+import pymysql
+
+conn = pymysql.connect(host='127.0.0.1', unix_socket='/tmp/mysql.sock',user='root', passwd='wawa1234', db='mysql')
+
+cur = conn.cursor(pymysql.cursors.DictCursor)
+cur.execute("USE shop")
+cur.execute("SELECT * FROM variations where is_listed = null")
+rows = cur.fetchall()
+
+with open('newcolorsized.csv', 'w', newline='') as file:
+    fieldnames = ['商品管理番号', '並び順', 'サイズ名称', '検索用サイズ', '色名称', '色系統', '在庫ステータス', '幅', '高さ', 'マチ']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in rows:
+        writer.writerow({'商品管理番号': row['item_id'], '並び順': row['bm_order'], 'サイズ名称': row['size_name'],
+        '検索用サイズ': row['bm_searchsize'], '色名称': row['bm_col_name'], '色系統': row['bm_col_family'], '在庫ステータス': row['has_stock'],
+        '幅': 22.5, '高さ': 16.5, 'マチ': 9.5})
+
+# fetch variations unavailable, yet listed on bm
+sql = "SELECT Variations.*, listed_items.buyma_id FROM Variations, listed_items \
+WHERE listed_items.item_id = Variations.item_id AND has_stock = 1 AND \
+availability NOT IN ( 'In Stock', 'Low in Stock' )"
+cur.execute(sql)
+rows = cur.fetchall()
+
+with open('colsize_outofstock.csv', 'w', newline='') as file:
+    fieldnames = ['商品ID','並び順','サイズ名称','検索用サイズ','色名称','色系統','在庫ステータス']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in rows:
+        # update 在庫ステータス to 0
+        writer.writerow({'商品ID': row['buyma_id'], '並び順': row['bm_order'], 'サイズ名称': row['size_name'],
+        '検索用サイズ': row['bm_searchsize'], '色名称': row['bm_col_name'], '色系統': row['bm_col_family'], '在庫ステータス': 0})
+
+# fetch variations back to available, and not listed
+sql = "SELECT Variations.*, listed_items.buyma_id FROM Variations, listed_items \
+WHERE listed_items.item_id = Variations.item_id AND has_stock = 0 AND is_listed IS NOT NULL \
+AND availability IN ( 'In Stock', 'Low in Stock' )"
+cur.execute(sql)
+rows = cur.fetchall()
+
+fp = open('file.csv', 'w')
+myFile = csv.writer(fp)
+myFile.writerow(rows)
+fp.close()
+
+with open('colsize_backstock.csv', 'w', newline='') as file:
+    fieldnames = ['商品ID','並び順','サイズ名称','検索用サイズ','色名称','色系統','在庫ステータス']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in rows:
+        # update 在庫ステータス to 1
+        writer.writerow({'商品ID': row['buyma_id'], '並び順': row['bm_order'], 'サイズ名称': row['size_name'],
+        '検索用サイズ': row['bm_searchsize'], '色名称': row['bm_col_name'], '色系統': row['bm_col_family'], '在庫ステータス': 1})
