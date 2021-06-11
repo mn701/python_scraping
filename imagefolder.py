ff513b881a9a2cfa7b1ec528cb5e474d46a1f4c9
mn701 <46086846+mn701@users.noreply.github.com>
2021-06-11 13:55:04 +0800

    [added] white background with rounded corner

--- imagefolder.py
+++ imagefolder.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import os
 import shutil
 import logofolder
@@ -15,21 +15,25 @@ def create_top(images, brand):
         create_im_from_4(images, brand, 100).save('100.png')
         create_im_from_4(images, brand, 150).save('150.png')
         create_im_from_4(images, brand, 200).save('200.png')
+        create_im_from_4_white(images, brand, 100).save('100.png')
+        create_im_from_4_white(images, brand, 150).save('150.png')
+        create_im_from_4_white(images, brand, 200).save('200.png')
     elif len(images) == 3:
         create_im_3_ver(images, brand, 100).save('100v.png')
         create_im_3_ver(images, brand, 200).save('200v.png')
-        create_im_3_hor(images, brand, 100, 0).save('100h.png')
-        create_im_3_hor(images, brand, 200, 0).save('200h.png')
-        create_im_3_hor(images, brand, 250, 0).save('250h0.png')
-        create_im_3_hor(images, brand, 250, 1).save('250h1.png')
-        create_im_3_hor(images, brand, 250, 2).save('250h2.png')
-        create_im_3_hor(images, brand, 300, 0).save('300h.png')
+        create_im_3_hor(images, brand, 100).save('100h.png')
+        create_im_3_hor(images, brand, 200).save('200h.png')
+        create_im_3_hor(images, brand, 250).save('250h.png')
+        create_im_3_hor(images, brand, 300).save('300h.png')
+        create_im_3_sq(images, brand, 100).save('300sq.png')
     elif len(images) == 2:
         create_im_2_ver(images, brand, 100).save('100v.png')
         create_im_2_ver(images, brand, 200).save('200v.png')
         create_im_2_hor(images, brand, 100).save('100h.png')
         create_im_2_hor(images, brand, 200).save('200h.png')
         create_im_2_hor(images, brand, 250).save('250h.png')
+        create_im_2_hor(images, brand, 300).save('300h.png')
+        create_im_2_hor(images, brand, 350).save('350h.png')
     elif len(images) == 1:
         create_im_1(images[0], brand, 0).save('0.png')
         create_im_1(images[0], brand, 50).save('50.png')
@@ -75,6 +79,71 @@ def create_im_from_4(images, brand, crop_size=0):
 
     # Add logo at center
     new_im = add_logo_center(new_im, brand)
+
+    # Add shop logo
+    im_with_logo = add_shop_logo(new_im)
+
+    return im_with_logo
+
+def create_im_from_4_white(images, brand, crop_size=0):
+    MARGIN = 20
+    LOGO_HEIGHT_HALF = int(116/2)
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (255, 255, 255))
+    # Create a list for quaretersized images.
+    quarter_images = []
+    side = SQUARE_HALF - MARGIN * 2 - LOGO_HEIGHT_HALF
+    for imgfile in images:
+        copy_im = Image.open(imgfile).copy()
+        cropped_im = crop_bottom_square(copy_im, crop_size)
+
+        quartersized_im = cropped_im.resize((side, side))
+        quartersized_im = add_corners(quartersized_im)
+        quarter_images.append(quartersized_im)
+
+    # Create a list of coordinates.
+    coords = []
+    for left in [int(SQUARE_HALF / 2 - side / 2), SQUARE_HALF + int(SQUARE_HALF / 2 - side / 2)]:
+        for top in [0 + MARGIN, SQUARE_HALF + MARGIN + LOGO_HEIGHT_HALF]:
+            coords.append((left, top))
+
+    # Paste each quartersized image to new_im
+    for quarter_im in quarter_images:
+        new_im.paste(quarter_im, coords[quarter_images.index(quarter_im)], quarter_im)
+
+    # Add logo at center
+    new_im = add_logo_center(new_im, brand)
+
+    # Add shop logo
+    im_with_logo = add_shop_logo(new_im)
+
+    return im_with_logo
+
+def create_im_3_sq(images, brand, crop_size=0):
+    MARGIN = 20
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (255, 255, 255))
+    # Create a list for quaretersized images.
+    quarter_images = []
+    side = SQUARE_HALF - MARGIN * 2
+    for imgfile in images:
+        copy_im = Image.open(imgfile).copy()
+        cropped_im = crop_bottom_square(copy_im, crop_size)
+
+        quartersized_im = cropped_im.resize((side, side))
+        quartersized_im = add_corners(quartersized_im)
+        quarter_images.append(quartersized_im)
+
+    # Create a list of coordinates.
+    coords = []
+    coords.append((int(SQUARE_HALF - side / 2), MARGIN))
+    coords.append((int(SQUARE_HALF / 2 - side / 2), SQUARE_HALF))
+    coords.append((SQUARE_HALF + int(SQUARE_HALF / 2 - side / 2), SQUARE_HALF))
+
+    # Paste each quartersized image to new_im
+    for quarter_im in quarter_images:
+        new_im.paste(quarter_im, coords[quarter_images.index(quarter_im)], quarter_im)
+
+    # Add logo at center
+    new_im = add_logo_topleft(new_im, brand)
 
     # Add shop logo
     im_with_logo = add_shop_logo(new_im)
@@ -143,9 +212,15 @@ def create_im_3_ver(images, brand, crop_x_size=100):
 
     return im_with_logo
 
-def create_im_2_hor(images, brand, crop_y_size=100):
-    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE),(236, 236, 236))
-    new_height = SQUARE_HALF
+def create_im_2_hor(images, brand, crop_y_size):
+    MARGIN = 50
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (255, 255, 255))
+    # if bg_color == 1:
+    #     new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (236, 236, 236))
+    # elif bg_color == 2:
+    #     new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (233, 229, 228))
+
+    new_height = int(SQUARE_HALF - 112/2 - MARGIN * 2)
     # Create a new folder to save resized images.
     os.makedirs('hresized', exist_ok=True)
 
@@ -155,13 +230,16 @@ def create_im_2_hor(images, brand, crop_y_size=100):
 
     # Create a list of coordinates.
     coords = []
-    for top in [0, SQUARE_HALF]:
+    for top in [0 + MARGIN, SQUARE_HALF + MARGIN + int(112/2)]:
         left = SQUARE_HALF - int(new_width / 2)
         coords.append((left, top))
 
     # Paste each quartersized image to new_im
     for im in resized_images:
-        new_im.paste(im, coords[resized_images.index(im)])
+        new_im.paste(im, coords[resized_images.index(im)], im)
+
+    # save
+    new_im.save(os.path.join('hresized', 'wologo.png'))
 
     # Add logo at center
     new_im = add_logo_center(new_im, brand)
@@ -171,30 +249,61 @@ def create_im_2_hor(images, brand, crop_y_size=100):
 
     return im_with_logo
 
-def create_im_3_hor(images, brand, crop_y_size, bg_color):
-    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE))
-    if bg_color == 1:
-        new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (236, 236, 236))
-    elif bg_color == 2:
-        new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (233, 229, 228))
-    new_height = int(SQUARE_SIZE / 3)
+def create_im_3_hor(images, brand, crop_y_size):
+    MARGIN = 10
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (255, 255, 255))
+    new_height = int((SQUARE_SIZE - MARGIN * 4)/ 3)
+
     # Create a new folder to save resized images.
     os.makedirs('hresized', exist_ok=True)
 
     # Create a list for three horizontal images.
-    resized_images = get_resized_images_h(images, crop_y_size, new_height)
+    resized_images = get_resized_images_h(images, crop_y_size - MARGIN, new_height)
     new_width = resized_images[0].size[0]
 
     # Create a list of coordinates.
     coords = []
-    for top in [0, new_height, int(new_height * 2)]:
+    for top in [MARGIN, int(new_height + MARGIN * 2), int(new_height * 2 + MARGIN * 3)]:
         left = 600
         coords.append((left, top))
 
     # Paste each quartersized image to new_im
     for im in resized_images:
-        new_im.paste(im, coords[resized_images.index(im)])
+        new_im.paste(im, coords[resized_images.index(im)], im)
 
+    # save
+    new_im.save(os.path.join('hresized', 'wologo.png'))
+
+    # Add logo on the left
+    new_im = add_logo_left(new_im, brand)
+
+    # Add shop logo
+    im_with_logo = add_shop_logo(new_im)
+
+    return im_with_logo
+
+def create_im_3_hor_len(images, brand, crop_y_size, len_crop, bg_color):
+    MARGIN = 10
+    new_im = Image.new('RGBA', (SQUARE_SIZE, SQUARE_SIZE), (255, 255, 255))
+    new_height = int((SQUARE_SIZE - MARGIN * 4)/ 3)
+    # Create a new folder to save resized images.
+    os.makedirs('hresized', exist_ok=True)
+
+    # Create a list for three horizontal images.
+    resized_images = get_resized_images_h_len(images, crop_y_size, len_crop, new_height)
+    new_width = resized_images[0].size[0]
+
+    # Create a list of coordinates.
+    coords = []
+    for top in [MARGIN, int(new_height + MARGIN * 2), int(new_height * 2 + MARGIN * 3)]:
+        left = 600
+        coords.append((left, top))
+
+    # Paste each quartersized image to new_im
+    for im in resized_images:
+        new_im.paste(im, coords[resized_images.index(im)], im)
+
+    new_im.save(os.path.join('hresized', 'nobg.png'))
     # Add logo on the left
     new_im = add_logo_left(new_im, brand)
 
@@ -224,6 +333,20 @@ def get_resized_images_h(images, crop_y_size, new_height):
         new_width = int(new_height * cropped_width / cropped_height)
         resized_im = cropped_im.resize((new_width, new_height))
         resized_im.save(os.path.join('hresized', imgfile))
+        resized_im = add_corners(resized_im)
+        resized_images.append(resized_im)
+    return resized_images
+
+def get_resized_images_h_len(images, crop_y_size, len_crop, new_height):
+    resized_images = []
+    for imgfile in images:
+        copy_im = Image.open(imgfile).copy()
+        cropped_im = crop_y(copy_im, crop_y_size, len_crop)
+        cropped_width, cropped_height = cropped_im.size
+        new_width = int(new_height * cropped_width / cropped_height)
+        resized_im = cropped_im.resize((new_width, new_height))
+        resized_im.save(os.path.join('hresized', imgfile))
+        resized_im = add_corners(resized_im)
         resized_images.append(resized_im)
     return resized_images
 
@@ -233,9 +356,9 @@ def crop_x(im, x_crop):
     cropped_im = im.crop(crop_rectangle)
     return cropped_im
 
-def crop_y(im, y_crop):
+def crop_y(im, y_crop, len_crop):
     x, y = im.size
-    crop_rectangle = (0, y_crop, x, int(y - y_crop))
+    crop_rectangle = (0, y_crop, x, int(y_crop + len_crop))
     cropped_im = im.crop(crop_rectangle)
     return cropped_im
 
@@ -245,15 +368,6 @@ def crop_y_bottom(im, crop_size):
     square_im = im.crop(crop_square)
     sq_x, sq_y = square_im.size
     crop_rectangle = (0, crop_size, sq_x, int(sq_y - 100))
-    cropped_im = square_im.crop(crop_rectangle)
-    return cropped_im
-
-def crop_y_bottom_4(im, crop_size):
-    x, y = im.size
-    crop_square = (0, y - x, x, y)
-    square_im = im.crop(crop_square)
-    sq_x, sq_y = square_im.size
-    crop_rectangle = (0, crop_size, sq_x, int(sq_y - crop_size*0.4))
     cropped_im = square_im.crop(crop_rectangle)
     return cropped_im
 
@@ -322,6 +436,21 @@ def add_logo_left(image, brand):
 
     return image
 
+def add_logo_topleft(image, brand):
+    # Adding logo image
+    if brand == 1:
+        logo_im = Image.open(logofolder.CK_LOGO).convert("RGBA")
+        logo_im = logo_im.resize((300, 300))
+        # Paste logo on botom left
+        image.paste(logo_im, (0, 20), logo_im)
+    else:
+        logo_im = Image.open(logofolder.PEDRO_LOGO).convert("RGBA")
+        logo_im = logo_im.resize((600, 112))
+        # Paste logo on botom left
+        image.paste(logo_im, (15, 20), logo_im)
+
+    return image
+
 def add_cklogo_top(image):
     # Adding logo image
     logo_im = Image.open(logofolder.CK_LOGO).convert("RGBA")
@@ -334,6 +463,19 @@ def add_cklogo_top(image):
 
     return image
 
+def add_corners(im):
+    rad = 50
+    circle = Image.new('L', (rad * 2, rad * 2), 0)
+    draw = ImageDraw.Draw(circle)
+    draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
+    alpha = Image.new('L', im.size, 255)
+    w, h = im.size
+    alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
+    alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
+    alpha.paste(circle.crop((rad, 0, rad * 2, rad)), (w - rad, 0))
+    alpha.paste(circle.crop((rad, rad, rad * 2, rad * 2)), (w - rad, h - rad))
+    im.putalpha(alpha)
+    return im
 
 def get_images(brand):
     images = []
