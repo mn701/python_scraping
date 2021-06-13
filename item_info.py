12f4b1605606d0ae9a921cb41a2a4a810796cbce
mn701 <46086846+mn701@users.noreply.github.com>
2021-06-13 13:36:11 +0800

    moved classes to folder

--- item_info.py
+++ item_info.py
@@ -11,6 +11,7 @@ import logging
 import math
 import json
 from db_config_file import db_config
+from classes.myclasses import Item, Variation, Buyer_price
 
 # #logging
 log_format = '%(asctime)s %(filename)s: %(message)s'
@@ -128,43 +129,6 @@ class DBHelper:
         sql =  "INSERT INTO Buyer_price(item_id, buyer, price, url) VALUES ('" + \
         buyer_price.item_id + "','" + buyer_price.buyer + "','" + buyer_price.price + "','" + buyer_price.url + "')"
         self.execute_insert(sql, 'buyer_price', buyer_price.url)
-
-# Holds information about an Item  -> tbl Items
-class Item:
-    def __init__(self, brand_id, serial, url, item_name, price, original_price, sale_info, description, details, season, listed):
-        self.brand_id = brand_id
-        self.serial = serial
-        self.url = url
-        self.item_name = item_name
-        self.price = price
-        self.original_price = original_price
-        self.sale_info = sale_info
-        self.description = description
-        self.details = details
-        self.season = season
-        self.listed = listed
-
-# Holds information about a Variaion of an Item  -> tbl Variations
-class Variation:
-    def __init__(self, item_id, sku, url, color_code, size_name, availability, has_stock, bm_col_name, bm_col_family, size_info):
-        self.item_id =  item_id
-        self.sku = sku
-        self.url = url
-        self.color_code = color_code
-        self.size_name = size_name
-        self.availability = availability
-        self.has_stock = has_stock
-        self.bm_col_name = bm_col_name
-        self.bm_col_family = bm_col_family
-        self.size_info = size_info
-
-# Holds information about listing from other buyers for an Item  -> tbl Buyer_price
-class Buyer_price:
-    def __init__(self, item_id, buyer, price, url):
-        self.item_id = item_id
-        self.buyer = buyer
-        self.price = price
-        self.url = url
 
 def getItemInfo(url, brand_id):
     try:
