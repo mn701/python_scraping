9a421ff6249c63370abeb302e249570d37c1e99a
mn701 <46086846+mn701@users.noreply.github.com>
2021-06-13 11:10:31 +0800

    [added] classes

--- item_info.py
+++ item_info.py
@@ -106,6 +106,36 @@ class DBHelper:
             return exist['id']
         return None
 
+
+# Holds information about an Item  -> tbl Items
+class Item:
+    def __init__(self, brand_id, serial, url, item_name, price, original_price, sale_info, description, details, season, listed):
+        self.brand_id = brand_id
+        self.serial = serial
+        self.url = url
+        self.item_name = item_name
+        self.price = price
+        self.original_price = original_price
+        self.sale_info = sale_info
+        self.description = description
+        self.details = details
+        self.season = season
+        self.listed = listed
+
+# Holds information about a Variaion of an Item  -> tbl Variations
+class Variation:
+    def __init__(self, item_id, sku, url, color_code, size_name, availability, has_stock, bm_col_name, bm_col_family, size_info):
+        self.item_id =  item_id
+        self.sku = sku
+        self.url = url
+        self.color_code = color_code
+        self.size_name = size_name
+        self.availability = availability
+        self.has_stock = has_stock
+        self.bm_col_name = bm_col_name
+        self.bm_col_family = bm_col_family
+        self.size_info = size_info
+
 def getItemInfo(url, brand_id):
     try:
         html = urlopen(url)
